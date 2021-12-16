5c324c054ad8b5b98619bb4a44c751490f0fad0e
salimM-code <mustafasalim000@gmail.com>
2021-12-16 18:45:45 -0500

    logic for call list

--- src/components/CallLists.jsx
+++ src/components/CallLists.jsx
@@ -1,27 +1,30 @@
 import React from "react";
 import { format } from 'date-fns'
 import { Link } from "react-router-dom";
-// import CallMadeIcon from '@mui/icons-material/CallMade';
-// import CallReceivedIcon from '@mui/material/Icon';
-// import CallMadeIcon from '@mui/material/Icon/'
+import { Wrapper } from "./styles/Lists.styled";
+
+
 
 const CallLists = (props) => {
   const { calls } = props;
 
 
   return ( 
-    <div>
+    <Wrapper>
+      <p className="note">Activity</p>
       {calls.map((call) => (
         <div key={call.id}>
-          {call.is_archived ? null : <Link to={`/call/${call.id}`}>
-            <h4>{`${call.direction === 'outbound' ? '↗️' : '↙️'} || ${call.to}`}</h4>
+        {call.is_archived ? null : <div className="call">
+          <Link to={`/call/${call.id}`}>
+            <h4 className={`to ${call.call_type === "missed" ? 'missed' : ''}`}>{`${call.direction === 'outbound' ? '↗️' : '↙️'} ${call.to === null ? 'Unknown' : call.to}`}</h4>
             <p>{`${call.from} - ${call.via}`}</p>
-            <div>{format(new Date(call.created_at), 'dd/MM HH:mm')}</div>
-          </Link>}
-          
+            <div className="date">{format(new Date(call.created_at), 'dd/MM HH:mm')}</div>
+          </Link>
+          </div>
+        }
         </div>
       ))}
-    </div>
+    </Wrapper>
   );
 }
 export default CallLists;
