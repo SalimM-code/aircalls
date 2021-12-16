785bb7a1dcebbc0f25affbb8eb13440a1b9851d5
salimM-code <mustafasalim000@gmail.com>
2021-12-16 18:43:50 -0500

    created custom Hook

--- src/hook/useAxios.js
+++ src/hook/useAxios.js
@@ -1,11 +1,30 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-const useAxios = (url) => {
-  const [arc, setArc] = useState(false);
+export const useAxios = () => {
+  const [calls, setCalls] = useState([]);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    axios.post(url)
-    
-  }, [url])
-}
+    axios
+      .get("https://aircall-job.herokuapp.com/activities")
+      .then((res) => {
+        if (res.status !== 200) {
+          throw Error("could not get data");
+        }
+        return res.data;
+      })
+      .then((data) => {
+        setError(null);
+        setCalls(data);
+        setIsPending(false);
+      })
+      .catch((err) => {
+        setIsPending(false);
+        setError(err.message);
+      });
+  }, []);
+
+  return { calls, isPending, error };
+};
